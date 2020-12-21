e6e4fc0811befeb30c3237c3a3806c0d40b949d5
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-12-21 10:44:33 +0700

    Add project files.

---
+++ ServerConfigRest_018_RamadhanFirdausN/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_018_RamadhanFirdausN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_018_RamadhanFirdausN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            ServiceHost hostobjek = null;
+
+            try
+            {
+                hostobjek = new ServiceHost(typeof(TI_UMY));
+        hostobjek.Open();
+                Console.WriteLine("Server ready...");
+                Console.ReadLine();
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
